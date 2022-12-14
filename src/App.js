6a63bebde203e1a2e804ebc9fac98154694b113f
Nikolay Dolgov <koly201020@yandex.ru>
2022-12-14 21:51:09 +0300

    Add a countdown

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import './App.css';
+import { useState } from 'react';
+import { ThemeContext } from './contexts/ThemeContext';
 import Header from './components/header/Header';
 import Clock from './components/clock/Clock';
 
 function App() {
+
+  const [useTheme, setUseTheme] = useState('day');
+  
+  const changeTheme = (theme) => {
+    setUseTheme(theme)
+  }
+
   return (
     <div className="App">
-      <Header />
-      <Clock />
+      <ThemeContext.Provider value={useTheme}>
+        <Header />
+        <Clock changeTheme={changeTheme}/>
+      </ThemeContext.Provider>
     </div>
   );
 }
--- src/components/clock/Clock.js
+++ src/components/clock/Clock.js
@@ -1,31 +1,62 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { ThemeContext } from '../../contexts/ThemeContext';
 import './clock.css';
 
 const Clock = () => {
 
-  const [useChange, setUseChange] = useState(1);
+  const theme = React.useContext(ThemeContext); /* тема - прописать дальше */
+
+  const [useChange, setUseChange] = useState(4);
+
+  const [useTime, setUseTime] = useState(4*60);
+
+  const [useTimer, setUseTimer] = useState(false);
+
+  let timer;
+
+  useEffect(() => {
+    if(useTimer === true && useTime > 0) {
+      const interval = setInterval(() => setUseTime(useTime => useTime - 1), 1000);
+      return () => clearInterval(interval);
+    }
+}, [useTimer, useTime]);
 
   const changeState = (change) => {
-    setUseChange(change)
+    setUseChange(change);
+    if(change === 4) {
+      setUseTime(4*60);
+    }
+    else {
+      setUseTime(1*60);
+    }
+  }
+
+  const changeTime = () => {
+    setUseTime(useTime - 1)
+  }
+
+  const start = () => {
+    if(!useTimer) {
+      /*timer = setInterval(changeTime, 1000);*/
+    }
+    setUseTimer(true);
   }
 
   return (
     <div className='clock'>
-      <input defaultValue={'player 1'} className='clock__input'/>
-      <input defaultValue={'player 2'} className='clock__input'/>
+      <input maxLength='20' placeholder='player 1' className='clock__input'/>
+      <input maxLength='20' placeholder='player 2' className='clock__input'/>
       <div className='clock__dial'>
-        <p className='clock__dia_text'>4:00</p>
+        <p className='clock__dia_text'>{`${Math.trunc(useTime/60)}:${useTime - Math.trunc(useTime/60)*60}`}</p>
       </div>
       <div className='clock__dial'>
-        <p className='clock__dia_text'>4:00</p>
+        <p className='clock__dia_text'>{`${Math.trunc(useTime/60)}:${useTime - Math.trunc(useTime/60)*60}`}</p>
       </div>
-      <button className='clock__button-main'>start</button>
-      <button className={useChange === 0 ? 'clock__button-average clock__button-average_active clock__button-average_position_left' : 'clock__button-average clock__button-average_position_left'} onClick={() => changeState(0)}>1 min</button>
-      <button className={useChange === 1 ? 'clock__button-average clock__button-average_active' : 'clock__button-average'} onClick={() => changeState(1)}>4 min</button>
-      <button className={useChange === 2 ? 'clock__button-average clock__button-average_active clock__button-average_position_right' : 'clock__button-average clock__button-average_position_right'} onClick={() => changeState(2)}>set</button>
-      <button className='clock__button-mini clock__button-mini_image_pause clock__button-mini_position_left'></button>
-      <button className='clock__button-mini clock__button-mini_image_repeat'></button>
-      <button className='clock__button-mini clock__button-mini_image_settings'></button>
+      <button className='clock__button-main clock__button-main_active' onClick={start}>start</button>
+      <button className='clock__button-main'>switch</button>
+      <button className={useChange === 1 ? 'clock__button-mini clock__button-mini_active clock__button-mini_position_left' : 'clock__button-mini clock__button-mini_position_left'} onClick={() => changeState(1)}>1 min</button>
+      <button className={useChange === 4 ? 'clock__button-mini clock__button-mini_active' : 'clock__button-mini'} onClick={() => changeState(4)}>4 min</button>
+      <button className='clock__button-mini clock__button-mini_image_desktop'></button>
       <button className='clock__button-mini clock__button-mini_image_dark-mode clock__button-mini_position_right'></button>
     </div>
   )
