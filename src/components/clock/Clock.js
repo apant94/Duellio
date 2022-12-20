ff8ad167b4582c3275241e2007e2599438fa6172
Nikolay Dolgov <koly201020@yandex.ru>
2022-12-21 00:07:25 +0300

    To make up a light and dark theme

--- src/components/clock/Clock.js
+++ src/components/clock/Clock.js
@@ -1,4 +1,4 @@
-import React, {useRef, useState, useEffect } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { ThemeContext } from '../../contexts/ThemeContext';
 import './clock.css';
 
@@ -49,7 +49,7 @@ const Clock = (props) => {
   }
 
   const switchButton = () => {
-    if(useTimerLeftActive){
+    if (useTimerLeftActive) {
       setUseTimerLeftActive(false);
       setUseTimerRightActive(true);
     }
@@ -70,29 +70,71 @@ const Clock = (props) => {
 
   return (
     <section className={theme === 'day' ? 'clock' : 'clock clock_dark'}>
-      {props.newYear && 
+      {/*props.newYear && 
         <input placeholder='Введите URL адрес до изображения' className={theme === 'day' ? 'clock__input clock__input_new-year' : 'clock__input clock__input_new-year clock__input_dark'} onKeyDown={(e) =>props.listenButton(e, inputRef)} ref={inputRef}/>
-      }
-      <input maxLength='20' placeholder='player 1' className={theme === 'day' ? 'clock__input' : 'clock__input clock__input_dark'}/>
-      <input maxLength='20' placeholder='player 2' className={theme === 'day' ? 'clock__input' : 'clock__input clock__input_dark'}/>
-      <div className={useTimerLeftActive ? useTimeLeft === 0 ? 'clock__dial clock__dial_active clock__dial-animation' : 'clock__dial clock__dial_active' : 'clock__dial'} onClick={switchButton} style={props.newYear ? {'background': '#FFFFFF80'} : {'background': '#FFFFFF'}}>
-        <p className='clock__dia_text'>
-          {`${Math.trunc(useTimeLeft / 60)}:`}
-          {useTimeLeft - Math.trunc(useTimeLeft / 60) * 60 <= 9 ? `0${useTimeLeft - Math.trunc(useTimeLeft / 60) * 60}` : `${useTimeLeft - Math.trunc(useTimeLeft / 60) * 60}`}
-        </p>
+      */}
+      <div className='clock__container-date'>
+        <input maxLength='20' placeholder='Player 1' className={theme === 'day' ? 'clock__input' : 'clock__input clock__input_dark'} />
+        <input maxLength='20' placeholder='Player 2' className={theme === 'day' ? 'clock__input' : 'clock__input clock__input_dark'} />
+        {/*<div className={useTimerLeftActive ? useTimeLeft === 0 ? 'clock__dial clock__dial_active clock__dial-animation' : 'clock__dial clock__dial_active' : 'clock__dial'} onClick={switchButton} style={props.newYear ? { 'background': '#FFFFFF80' } : { 'background': '#FFFFFF' }}> */}
+        <div className={useTimer ?
+          (useTimerLeftActive ?
+            (theme === 'day' ? 'clock__dial clock__dial_status_work' : 'clock__dial clock__dial_dark clock__dial_status_work clock__dial_status_work_dark')
+            :
+            (theme === 'day' ? 'clock__dial' : 'clock__dial clock__dial_dark'))
+          :
+          (useTimerLeftActive ?
+            (theme === 'day' ? 'clock__dial clock__dial_active' : 'clock__dial clock__dial_dark clock__dial_active clock__dial_active_dark')
+            :
+            (theme === 'day' ? 'clock__dial' : 'clock__dial clock__dial_dark')
+          )}
+          onClick={switchButton}>
+          <p className={useTimer ?
+            (useTimerLeftActive ?
+              (theme === 'day' ? 'clock__dia-text clock__dia-text_status_work' : 'clock__dia-text clock__dia-text_dark clock__dia-text_status_work clock__dia-text_status_work_dark')
+              :
+              (theme === 'day' ? 'clock__dia-text' : 'clock__dia-text clock__dia-text_dark'))
+            :
+            (theme === 'day' ? 'clock__dia-text' : 'clock__dia-text clock__dia-text_dark')}>
+            {`${Math.trunc(useTimeLeft / 60)}:`}
+            {useTimeLeft - Math.trunc(useTimeLeft / 60) * 60 <= 9 ? `0${useTimeLeft - Math.trunc(useTimeLeft / 60) * 60}` : `${useTimeLeft - Math.trunc(useTimeLeft / 60) * 60}`}
+          </p>
+        </div>
+        <div className={useTimer ?
+          (useTimerRightActive ?
+            (theme === 'day' ? 'clock__dial clock__dial_status_work' : 'clock__dial clock__dial_dark clock__dial_status_work clock__dial_status_work_dark')
+            :
+            (theme === 'day' ? 'clock__dial' : 'clock__dial clock__dial_dark'))
+          :
+          (useTimerRightActive ?
+            (theme === 'day' ? 'clock__dial clock__dial_active' : 'clock__dial clock__dial_dark clock__dial_active clock__dial_active_dark')
+            :
+            (theme === 'day' ? 'clock__dial' : 'clock__dial clock__dial_dark')
+          )}
+          onClick={switchButton}>
+          <p className={useTimer ?
+            (useTimerRightActive ?
+              (theme === 'day' ? 'clock__dia-text clock__dia-text_status_work' : 'clock__dia-text clock__dia-text_dark clock__dia-text_status_work clock__dia-text_status_work_dark')
+              :
+              (theme === 'day' ? 'clock__dia-text' : 'clock__dia-text clock__dia-text_dark'))
+            :
+            (theme === 'day' ? 'clock__dia-text' : 'clock__dia-text clock__dia-text_dark')}>
+            {`${Math.trunc(useTimeRight / 60)}:`}
+            {useTimeRight - Math.trunc(useTimeRight / 60) * 60 <= 9 ? `0${useTimeRight - Math.trunc(useTimeRight / 60) * 60}` : `${useTimeRight - Math.trunc(useTimeRight / 60) * 60}`}
+          </p>
+        </div>
       </div>
-      <div className={useTimerRightActive ? useTimeRight === 0 ? 'clock__dial clock__dial_active clock__dial-animation' : 'clock__dial clock__dial_active' : 'clock__dial'} onClick={switchButton}  style={props.newYear ? {'background': '#FFFFFF80'} : {'background': '#FFFFFF'}}>
-        <p className='clock__dia_text'>
-          {`${Math.trunc(useTimeRight / 60)}:`}
-          {useTimeRight - Math.trunc(useTimeRight / 60) * 60 <= 9 ? `0${useTimeRight - Math.trunc(useTimeRight / 60) * 60}` : `${useTimeRight - Math.trunc(useTimeRight / 60) * 60}`}
-        </p>
+      <div className='clock__container-button'>
+        <button className={useTimer ? 
+        (theme === 'day' ? 'clock__button-main' : 'clock__button-main clock__button-main_dark')
+        : 
+        (theme === 'day' ? 'clock__button-main clock__button-main_active' : 'clock__button-main clock__button-main_dark clock__button-main_active clock__button-main_active_dark')} onClick={() => startButton(!useTimer)}>{useTimer ? 'stop' : 'start'}</button>
+        <button className={theme === 'day' ? 'clock__button-main' : 'clock__button-main clock__button-main_dark'} onClick={switchButton}>switch</button>
+        <button className={theme === 'day' ? 'clock__button-mini' : 'clock__button-mini clock__button-mini_dark'} onClick={() => changeButtonTime(1)}>1 min</button>
+        <button className={theme === 'day' ? 'clock__button-mini' : 'clock__button-mini clock__button-mini_dark'} onClick={() => changeButtonTime(4)}>4 min</button>
+        <button className={theme === 'day' ? 'clock__button-mini' : 'clock__button-mini clock__button-mini_dark'}>set</button>
+        <button className={theme === 'day' ? 'clock__button-mini clock__button-mini_image_moon' : 'clock__button-mini clock__button-mini_dark clock__button-mini_image_sun'} onClick={themeButtonChange}></button>
       </div>
-      <button className='clock__button-main clock__button-main_active' onClick={() => startButton(!useTimer)}>{useTimer ? 'pause' : 'start'}</button>
-      <button className='clock__button-main' onClick={switchButton}>switch</button>
-      <button className={useChange === 1 ? 'clock__button-mini clock__button-mini_active clock__button-mini_position_left' : 'clock__button-mini clock__button-mini_position_left'} onClick={() => changeButtonTime(1)}>1 min</button>
-      <button className={useChange === 4 ? 'clock__button-mini clock__button-mini_active' : 'clock__button-mini'} onClick={() => changeButtonTime(4)}>4 min</button>
-      <button className='clock__button-mini clock__button-mini_image_desktop' onDoubleClick={() => props.newYearTheme(!props.newYear)}></button>
-      <button className='clock__button-mini clock__button-mini_image_dark-mode clock__button-mini_position_right' onClick={themeButtonChange}></button>
     </section>
   )
 }
