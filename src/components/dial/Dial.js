07c008f04495dfabb8388572e9fcaf708cc1dfd9
Nikolay Dolgov <koly201020@yandex.ru>
2022-12-23 23:47:13 +0300

    Add inputs

--- src/components/dial/Dial.js
+++ src/components/dial/Dial.js
@@ -6,7 +6,20 @@ import { ThemeContext } from '../../contexts/ThemeContext';
 const Dial = (props) => {
   const theme = React.useContext(ThemeContext);
 
+  const [inputValue, setInputValue] = useState(`${props.timeCount.minutes}:${props.timeCount.seconds}`);
+
+  const [field, setField] = useState(false);
+
   const [useActiveStyle, setUseActiveStyle] = useState(false);
+
+  const changeInputValue = (text) => {
+    if(text.length <=5)
+      setInputValue(text);
+  }
+
+  const changeField = (bool) => {
+    setField(bool);
+  }
 
   const changeActiveStyle = (state) => {
     setUseActiveStyle(state);
@@ -23,19 +36,37 @@ const Dial = (props) => {
         (theme === 'day' ? 'dial dial_active' : 'dial dial_view_dark dial_active dial_active_dark')
         :
         (theme === 'day' ? 'dial' : 'dial dial_view_dark')
-      )} ${useActiveStyle && !props.timer && (theme === 'day' ? 'dial_view_day-hover' : 'dial_view_dark-hover')}`}>
-      <p className={`${props.timer ?
-        (props.timerActive ?
-          (theme === 'day' ? 'dial__text dial__text_status_work' : 'dial__text dial__text_status_work_dark')
+      )} ${useActiveStyle && !props.timer && (theme === 'day' ? 'dial_view_day-hover' : 'dial_view_dark-hover')}`}
+      onClick={() => props.choice(props.modifier)}>
+      {!field ?
+        <p className={`${props.timer ?
+          (props.timerActive ?
+            (theme === 'day' ? 'dial__text dial__text_status_work' : 'dial__text dial__text_status_work_dark')
+            :
+            (theme === 'day' ? 'dial__text' : 'dial__text dial__text_view_dark'))
           :
-          (theme === 'day' ? 'dial__text' : 'dial__text dial__text_view_dark'))
+          (theme === 'day' ? 'dial__text' : 'dial__text text dial__text_view_dark')}
+          ${useActiveStyle && !props.timer && (theme === 'day' ? 'dial__text_view_day-hover' : 'dial__text_view_dark-hover')}`}
+          onMouseOver={() => changeActiveStyle(!useActiveStyle)}
+          onMouseLeave={() => changeActiveStyle(!useActiveStyle)}
+          onDoubleClick={() => changeField(true)}>
+          {`${props.timeCount.minutes}:${props.timeCount.seconds}`}
+        </p>
         :
-        (theme === 'day' ? 'dial__text' : 'dial__text text dial__text_view_dark')}
-        ${useActiveStyle && !props.timer && (theme === 'day' ? 'dial__text_view_day-hover' : 'dial__text_view_dark-hover')}`}
-        onMouseOver={() => changeActiveStyle(!useActiveStyle)}
-        onMouseLeave={() => changeActiveStyle(!useActiveStyle)}>
-        {`${props.timeCount.minutes}:${props.timeCount.seconds}`}
-      </p>
+        <input className={`${props.timer ?
+          (props.timerActive ?
+            (theme === 'day' ? 'dial__text dial__text_status_work' : 'dial__text dial__text_status_work_dark')
+            :
+            (theme === 'day' ? 'dial__text' : 'dial__text dial__text_view_dark'))
+          :
+          (theme === 'day' ? 'dial__text' : 'dial__text text dial__text_view_dark')}
+          ${useActiveStyle && !props.timer && (theme === 'day' ? 'dial__text_view_day-hover' : 'dial__text_view_dark-hover')}`}
+          style={{ width: (inputValue.length + 1) * 33 + 'px' }}
+          value={inputValue}
+          onChange={(e) => changeInputValue(e.target.value)}
+          onMouseOver={() => changeActiveStyle(!useActiveStyle)}
+          onMouseLeave={() => {changeActiveStyle(!useActiveStyle); changeField(false)}}/>
+      }
     </div>
   )
 }
