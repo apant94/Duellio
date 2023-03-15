80034104248ab3b1b9566fb7d91f069d85c65c72
Николай <78875139+NikolayDF@users.noreply.github.com>
2023-03-15 08:31:08 +0300

    Update Сases.js

--- src/components/cases/Сases.js
+++ src/components/cases/Сases.js
@@ -1,1 +1,14 @@
+import './cases.css';
 
+const Сases = () => {
+  return (
+    <div className='cases'>
+      <h2>База кейсов для переговорных поединков</h2>
+      <div>Поиск</div>
+      <div>Теги<div>
+      <div>Основная страница<div>
+    </div>
+  )
+}
+
+export default Сases;
