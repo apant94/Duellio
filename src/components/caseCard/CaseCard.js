b34ccdab27219589d8c78ec3e4c18977463c7821
Anna Panteleeva <apanteleeva94@gmail.com>
2023-04-13 14:21:02 +0300

    fix: рефактори код в cases

--- src/components/caseCard/CaseCard.js
+++ src/components/caseCard/CaseCard.js
@@ -1,19 +1,8 @@
 import './caseCard.css';
-import { useState } from 'react';
 import iconUnwrap from '../../images/unwrap.svg';
 import { Link } from 'react-router-dom';
 
 const CaseCard = ({title, text}) => {
-  const [hovered, setHovered] = useState(false); //стейт наведения мыши на кейс
-
-  // const handleMouseOver = () => {
-  //   setHovered(true);
-  // }
-
-  // const handleMouseOut = () => {
-  //   setHovered(false);
-  // }
-
   return(
     <Link 
       to='/casepage' 
