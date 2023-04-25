754fefad9a1629c2e3e01ea0c42d3443d40ed6c8
Anna Panteleeva <apanteleeva94@gmail.com>
2023-04-25 18:03:19 +0300

    fix: исправляй ширину footer

--- src/components/casesTagsList/CasesTagsList.js
+++ src/components/casesTagsList/CasesTagsList.js
@@ -1,20 +1,40 @@
 import './casesTagsList.css';
-import iconClock from '../../images/clockTag.svg';
-import moreClock from '../../images/moreTag.svg';
+import React, { useState } from 'react'
+import Select from 'react-select'
+// import iconClock from '../../images/clockTag.svg';
 import CasesTag from '../casesTag/CasesTag';
 
 const CasesTagsList = () => {
+  const options = [
+    { value: 'classic', label: '4 min' },
+    { value: 'express', label: '1 min' },
+  ];
+  const [chosenDuration, setChosenDuration] = useState('classic');
+
+  const getValue = () => {
+    return chosenDuration ? options.find(c => c.value === chosenDuration) : ''
+  };
+
+  const onChange = (newValue) => {
+    setChosenDuration(newValue.value);
+  }
+  
   const tags = ['Трудоустройство', 'Менеджмент', 'Семья', 'Продажи', 'Отношения', 'IT']; //список тэгов временно до появления бэка
 
   return(
     <div className='casestags'>
-        <select className='casestags__element'> 
-          {/* <img alt="Часы" src={iconClock} className='casestags__clock-icon' />
-          <p className='casestags__text'>4 min</p>
-          <img alt="Убрать" src={moreClock} className='casestags__more-icon' /> */}
+        <Select 
+          className="casestags-select__container"
+          classNamePrefix='casestags-select'
+          onChange={onChange} 
+          value={getValue()} 
+          options={options} 
+        />
+        {/* <select className='casestags__element'> 
+          <img alt="Часы" src={iconClock} className='casestags__clock-icon' />
           <option className='casestags__text'>4 min</option>
           <option className='casestags__text'>1 min</option>
-        </select>
+        </select> */}
         <CasesTag title={tags[0]} />
         <CasesTag title={tags[1]} />
         <CasesTag title={tags[2]} />
