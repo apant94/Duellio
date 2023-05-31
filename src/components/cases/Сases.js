2a7e9664e783406fdc549fa3661bfc9c887b39b7
Anna Panteleeva <apanteleeva94@gmail.com>
2023-05-31 21:12:35 +0300

    adaptive: добавляет мобилку

--- src/components/cases/Сases.js
+++ src/components/cases/Сases.js
@@ -5,21 +5,23 @@ import CasesTagsList from '../casesTagsList/CasesTagsList';
 
 const Cases = () => {
   return (
-    <section className='cases'>
-      <h2 className='cases__title'>База кейсов для переговорных поединков</h2>
-      <form className='cases__search-form'>
+    <section className="cases">
+      <h2 className="cases__title">
+        База кейсов для&nbsp;переговорных поединков
+      </h2>
+      <form className="cases__search-form">
         <input
           id="search"
           type="text"
           placeholder="Введите ключевые слова для поиска"
-          className='cases__search-form-input'
+          className="cases__search-form-input"
         />
-        <img alt="Поиск" src={iconSearch} className='cases__search-form-icon' />
+        <img alt="Поиск" src={iconSearch} className="cases__search-form-icon" />
       </form>
       <CasesTagsList />
       <CasesList />
     </section>
-  )
-}
+  );
+};
 
 export default Cases;
